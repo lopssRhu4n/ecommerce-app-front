e20ce5f3a99c966cbc1976022fb717883fcbc975
Rhuan Lopes <rhuanlopes535@gmail.com>
2023-07-07 19:13:34 -0300

    Adding interceptor for axios

--- src/http/clients/AxiosClient.ts
+++ src/http/clients/AxiosClient.ts
@@ -1,8 +1,25 @@
 import axios from 'axios';
+import { useAuthStore } from '@/stores/AuthStore';
+
+const authStore = useAuthStore();
 
 const baseURL = 'http://localhost/api';
 
-export default axios.create({
+const axiosInstance = axios.create({
   baseURL,
   headers: { 'Content-Type': 'application/json' },
 });
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    authStore.checkIsLogged();
+    const token = authStore.apiToken;
+    if (token) config.headers = { ...config.headers, Authentication: 'Bearer ' + token };
+    return config;
+  },
+  (error) => {
+    console.log(error);
+  },
+);
+
+export default axiosInstance;
